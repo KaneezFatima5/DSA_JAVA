0d763fde746dd11b79d1085490de2a0639358a74
Kaneez Fatima <kaneezfatimarajper55@gmail.com>
2024-05-05 12:14:59 +0500

    max profit part 2

--- DynamicProgramming/BestTimeToBuy.java
+++ DynamicProgramming/BestTimeToBuy.java
@@ -1,9 +1,11 @@
 package DynamicProgramming;
 public class BestTimeToBuy {
     public static void main(String args[]){
-        int[] arr={7,1,5,3,6,1,4};
-        System.out.print(maxProfit(arr));
+        int[] arr={7,1,5,3,6,4};
+        System.out.print(maxProfit2(arr));
     }
+
+    //easy 
     public static int maxProfit(int[] prices) {
         int buy=prices[0];
         int maxPro=0;
@@ -18,4 +20,26 @@ public class BestTimeToBuy {
         }
         return maxPro;
     }
+
+    //medium part-II
+    public static int maxProfit2(int[] prices) {
+        int min=prices[0];
+        int maxPro=0;
+        int currProfit=0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            if(currProfit<prices[i]-min){
+                currProfit=prices[i]-min;
+            }else{
+                maxPro=maxPro+currProfit;
+                currProfit=0;
+                min=prices[i];
+            } 
+        }
+        maxPro=maxPro+currProfit;
+        return maxPro;
+    }
+
 }
