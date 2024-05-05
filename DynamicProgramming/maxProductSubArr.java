3a76ccfb9fe0a266a96513d4bf78eda39011ffaa
Kaneez Fatima <kaneezfatimarajper55@gmail.com>
2024-05-05 15:48:02 +0500

    corrections

--- DynamicProgramming/maxProductSubArr.java
+++ DynamicProgramming/maxProductSubArr.java
@@ -2,27 +2,25 @@ package DynamicProgramming;
 
 public class maxProductSubArr {
     public static void main(String[] args){
-        int[] arr={-2,0,-1};
-        // int[] arr={2,3,-2,4};
+        // int[] arr={-2,0,-1};
+        int[] arr={-2,3,-4};
         int maxProduct=maxProduct(arr);
         System.out.print(maxProduct);
     }
     public static int maxProduct(int[] nums){
-        int i=1;
-        int max=nums[0];
+        int curmin=nums[0];
         int curMax=nums[0];
-        int curMin=nums[0];
-        while(i<nums.length ){
-            if(nums[i]>0){
-                max=Math.max(curMin, max);
-                curMin=1;
-                curMax=curMax*nums[i];
+        int max=curmin;
+        for(int i=1; i<nums.length; i++){
+            if(curmin<nums[i]){
+                curmin=curmin*nums[i];
+                curmin=nums[i];
             }else{
-                max=Math.max(curMax, max);
-                curMax=1;
-                curMin=curMin*nums[i];
+                
             }
-            i++;
+            if(max<curmin){
+                max=curmin;
+            }
         }
         return max;
     }
