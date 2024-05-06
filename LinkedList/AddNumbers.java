003497684e6c14c040ad1c0b68c85a890aee89db
Kaneez Fatima <kaneezfatimarajper55@gmail.com>
2024-05-06 12:33:21 +0500

    linked list sum

--- LinkedList/AddNumbers.java
+++ LinkedList/AddNumbers.java
@@ -19,21 +19,51 @@ public class AddNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode node1=l1;
         ListNode node2=l2;
-        ListNode node3= new ListNode();
+        ListNode l3 = new ListNode(-1);
+        ListNode node3=l3;
+        int rem=0;
         while (node1!=null && node2!=null) {
-            node3.val=node1.val+node2.val;
-            System.out.print(node3.val);
+            node3.next=new ListNode();
             node3=node3.next;
+            node3.val=(node1.val+node2.val+rem)%10;
+            System.out.print(l3.val);
+            if((node1.val+node2.val+rem)>9){
+                rem=1;
+            }else{
+                rem=0;
+            }
 
             node1=node1.next;
             node2=node2.next;
         }
-        if(node1!=null){
-            node3.next=node1;
+        while(node1!=null){
+            node3.next=new ListNode();
+            node3=node3.next;
+            node3.val=(node1.val+rem)%10;
+            if((node1.val+rem)>9){
+                rem=1;
+            }else{
+                rem=0;
+            }
+            node1=node1.next;
         }
-        if(node2!=null){
-            node3.next=node2;
+        while(node2!=null){
+            node3.next=new ListNode();
+            node3=node3.next;
+            node3.val=(node2.val+rem)%10;
+            if((node2.val+rem)>9){
+                rem=1;
+            }else{
+                rem=0;
+            }
+            node2=node2.next;
         }
-        return node3;
+        if(rem>0){
+            node3.next=new ListNode();
+            node3=node3.next;
+            node3.val=rem;
+        }
+        System.out.print(l3.val);
+        return l3.next;
     }
 }
