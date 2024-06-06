243e6fde727dd59e9ff09fc0b8ed6cfa2e752e0b
Kaneez Fatima <kaneezfatimarajper55@gmail.com>
2024-06-06 17:49:35 +0500

    add two numbers (lists)

--- LinkedList/AddNumbers.java
+++ LinkedList/AddNumbers.java
@@ -17,53 +17,92 @@ public class AddNumbers {
 
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode node1=l1;
-        ListNode node2=l2;
-        ListNode l3 = new ListNode(-1);
-        ListNode node3=l3;
+        ListNode head=new ListNode();
+        ListNode node=head;
         int rem=0;
-        while (node1!=null && node2!=null) {
-            node3.next=new ListNode();
-            node3=node3.next;
-            node3.val=(node1.val+node2.val+rem)%10;
-            System.out.print(l3.val);
-            if((node1.val+node2.val+rem)>9){
-                rem=1;
-            }else{
-                rem=0;
+        while(l1!=null && l2!=null){
+            int add = (l1.val+l2.val+rem);
+            node.val=add%10;
+            l1=l1.next;
+            l2=l2.next;
+            rem=add/10;
+            if(l1!=null || l2!=null){
+                node.next=new ListNode();
+                node=node.next;
             }
-
-            node1=node1.next;
-            node2=node2.next;
+            System.out.print(rem);
         }
-        while(node1!=null){
-            node3.next=new ListNode();
-            node3=node3.next;
-            node3.val=(node1.val+rem)%10;
-            if((node1.val+rem)>9){
-                rem=1;
-            }else{
-                rem=0;
+        while(l1!=null){
+            node.val=(l1.val+rem)%10;
+            rem=(l1.val+rem)/10;
+            l1=l1.next;
+            if(l1!=null){
+                node.next=new ListNode();
+                node=node.next;
             }
-            node1=node1.next;
         }
-        while(node2!=null){
-            node3.next=new ListNode();
-            node3=node3.next;
-            node3.val=(node2.val+rem)%10;
-            if((node2.val+rem)>9){
-                rem=1;
-            }else{
-                rem=0;
+        while(l2!=null){
+            node.val=(l2.val+rem)%10;
+            rem=(l2.val+rem)/10;
+            l2=l2.next;
+            if(l2!=null){
+                node.next=new ListNode();
+                node=node.next;
             }
-            node2=node2.next;
         }
         if(rem>0){
-            node3.next=new ListNode();
-            node3=node3.next;
-            node3.val=rem;
+            node.next=new ListNode();
+            node=node.next;
+            node.val=rem;
         }
-        System.out.print(l3.val);
-        return l3.next;
+        return head;
     }
+    // ListNode node1=l1;
+    //     ListNode node2=l2;
+    //     ListNode l3 = new ListNode(-1);
+    //     ListNode node3=l3;
+    //     int rem=0;
+    //     while (node1!=null && node2!=null) {
+    //         node3.next=new ListNode();
+    //         node3=node3.next;
+    //         node3.val=(node1.val+node2.val+rem)%10;
+    //         System.out.print(l3.val);
+    //         if((node1.val+node2.val+rem)>9){
+    //             rem=1;
+    //         }else{
+    //             rem=0;
+    //         }
+
+    //         node1=node1.next;
+    //         node2=node2.next;
+    //     }
+    //     while(node1!=null){
+    //         node3.next=new ListNode();
+    //         node3=node3.next;
+    //         node3.val=(node1.val+rem)%10;
+    //         if((node1.val+rem)>9){
+    //             rem=1;
+    //         }else{
+    //             rem=0;
+    //         }
+    //         node1=node1.next;
+    //     }
+    //     while(node2!=null){
+    //         node3.next=new ListNode();
+    //         node3=node3.next;
+    //         node3.val=(node2.val+rem)%10;
+    //         if((node2.val+rem)>9){
+    //             rem=1;
+    //         }else{
+    //             rem=0;
+    //         }
+    //         node2=node2.next;
+    //     }
+    //     if(rem>0){
+    //         node3.next=new ListNode();
+    //         node3=node3.next;
+    //         node3.val=rem;
+    //     }
+    //     System.out.print(l3.val);
+    //     return l3.next;
 }
