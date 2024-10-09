505afe2126168bac99c35810161dfd81f6774fc4
Kaneez Fatima <kaneezfatimarajper55@gmail.com>
2024-10-09 16:02:09 +0500

    sql problem #1907 resolved using union

--- postgreSql/queries.sql
+++ postgreSql/queries.sql
@@ -22,3 +22,10 @@ with person as (
     select person_id, person_name, weight, sum(weight) over (order by turn) as cum_weight, turn from Queue order by turn
 )
 select person_name from person where cum_weight<=1000 order by turn desc limit 1
+
+-- Leetcode problem #1907 Count Salary Categories
+select 'Low Salary' as category, count(account_id) as accounts_count from Accounts where income<20000
+UNION
+select 'Average Salary' as category, count(account_id) as accounts_count from Accounts where income>=20000 and income<=50000
+UNION
+select 'High Salary' as category, count(account_id) as accounts_count from Accounts where income>50000
